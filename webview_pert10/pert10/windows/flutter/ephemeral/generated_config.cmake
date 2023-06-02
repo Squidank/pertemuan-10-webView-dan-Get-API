d175542a7ff9ea4698df8d869fb80ee4fa013485
Squidank <123918569+Squidank@users.noreply.github.com>
2023-06-02 18:53:55 +0700

    Add files via upload

---
+++ webview_pert10/pert10/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\asus\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\semester 6\\Mobile Programing\\webview_pert10" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\asus\\flutter"
+  "PROJECT_DIR=C:\\semester 6\\Mobile Programing\\webview_pert10"
+  "FLUTTER_ROOT=C:\\Users\\asus\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\semester 6\\Mobile Programing\\webview_pert10\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\semester 6\\Mobile Programing\\webview_pert10"
+  "FLUTTER_TARGET=C:\\semester 6\\Mobile Programing\\webview_pert10\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\semester 6\\Mobile Programing\\webview_pert10\\.dart_tool\\package_config.json"
+)
